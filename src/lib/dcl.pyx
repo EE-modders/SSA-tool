2cb368abbde0d077d72852eb9f140557b64f35a6
zocker-160 <zocker1600@posteo.net>
2022-08-30 14:20:06 +0200

    remove old nonsense

--- src/lib/dcl.pyx
+++
@@ -1,7 +0,0 @@
-
-cdef extern from "libblast.c":
-    int decompress_bytes(unsigned long l, char* inbuff, char* outf)
-
-
-cpdef int decompress(unsigned long l, char* inbuff, char* outf):
-    return decompress_bytes(l, inbuff, outf)
--- src/lib/dclgen.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import Extension, setup
-from Cython.Build import cythonize
-
-ext = Extension(
-    name="cdcl",
-    sources=["dcl.pyx"]
-)
-
-setup(ext_modules=cythonize(ext, language_level=3))
